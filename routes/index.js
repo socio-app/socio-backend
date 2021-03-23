acba89ff128a6d872a61b71ce9b8f06e80efc611
okipratama97 <okipratama97@gmail.com>
2021-03-23 19:01:32 +0700

    fixed upload

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 const { Router } = require('express')
 const router = Router()
 const userRoute = require('./userRoute')
-const ImageController = require('../controllers/ImageController.js')
-// const missionRoute = require('./missionRoute')
 
 const { upload } = require('../middlewares/multer')
 
 router.use('/users', userRoute)
-// router.use('/missions', missionRoute)
-router.post('/images', upload, ImageController.uploadImage)
 
 module.exports = router
